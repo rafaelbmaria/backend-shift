cfb06f768294a61c4d69ff10c5e98f60c50380a3
Rogerio de Morais <rogerio.morais@koin.com.br>
2023-05-08 21:38:28 -0300

    Criacao da classe Marca usando lombok

---
+++ src/main/java/br/com/fiap/rentacar/entity/Marca.java
@@ -0,0 +1,13 @@
+package br.com.fiap.rentacar.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Marca {
+
+    private Long id;
+    private String nome;
+
+}
